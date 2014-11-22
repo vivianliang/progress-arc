bb81db9799c9c9f771a32ac5e6cfd533dfdbc50c
vivianliang <vliang88@gmail.com>
2014-11-22 03:10:10 -0600

    combine arcs together as expected. fixes #1

--- app/app.js
+++ app/app.js
@@ -10,54 +10,20 @@ progressIndicatorApp.directive('ngArc', function(){
 	return {
 		link: function (scope, element,attrs){
 
-			var width= 105,
-				height=105,
+			var width = 200,
+				height = 200,
 				τ = 2 * Math.PI;				
 
-			// Expected
-			
-			var arc = d3.svg.arc()
-				.innerRadius(40)
-				.outerRadius(50)
-				.startAngle(0);
-			var svg = d3.select(element[0]).append("svg")
-			    .attr("width", width)
-			    .attr("height", height)
-			  .append("g")
-			    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")")
-			var background = svg.append("path")
-			    .datum({endAngle: τ})
-			    .style("fill", "#ddd")
-			    .attr("d", arc);
-			var foreground = svg.append("path")
-			    .datum({endAngle: 0 * τ})
-			    .style("fill", "#6CBB3C")
-			    .attr("d", arc);
-
-			svg.append("text")
-				.text(function(d){
-					return attrs.expected*100 + "\%";
-				})
-				.attr("font-size","30px")
-				.attr("text-anchor", "middle")
-				.attr("y",+20)
-			svg.append("text").text("expected")
-				.attr("text-anchor", "middle")
-				.attr("y",-10);
-
-			foreground.transition().duration(1000).call(arcTween, attrs.expected*τ);
-			
-			// Actual
-
 			var arc_expected = d3.svg.arc()
-			    .innerRadius(30)
-			    .outerRadius(40)
+			    .innerRadius(40)
+			    .outerRadius(45)
 			    .startAngle(0);
 
 			var arc_actual = d3.svg.arc()
-			    .innerRadius(40)
-			    .outerRadius(50)
+			    .innerRadius(47)
+			    .outerRadius(55)
 			    .startAngle(0);
+
 			var svg_actual = d3.select(element[0]).append("svg")
 			    .attr("width", width)
 			    .attr("height", height)
@@ -66,24 +32,27 @@ progressIndicatorApp.directive('ngArc', function(){
 
 			var cir = svg_actual.append("circle")
 				.attr("fill", "#E6E6E6")
-				.attr("r", width/3);
+				.attr("r", width/6);
 
-			var background_actual = svg_actual.append("path")
-			    .datum({endAngle: τ})
-			    .style("fill", "#ddd")
-			    .attr("d", arc_actual);
 
 			/*
 			 * The colors of the outer ring should change to orange or red
 			 * when the actual is more than 25% or 50% behind expected.
 			*/
 
+			var foreground_expected = svg_actual.append("path")
+				.datum({endAngle: .9 * τ})
+			    .style("fill", function(d){
+					return "#6cbb3c";
+			    })
+			    .attr("d", arc_expected);
+
 			var foreground_actual = svg_actual.append("path")
 			    .datum({endAngle: 0 * τ})
 			    .style("fill", function(d){
 			    	if (attrs.actual  < attrs.expected*.75 && attrs.actual >= attrs.expected*.50){
 			    		return "orange";
-			    	} else if (attrs.actual/attrs.expected < .50){
+			    	} else if (attrs.actual < attrs.expected*.50){
 			    		return "red";
 			    	} else{
 			    		return "green";
@@ -95,24 +64,35 @@ progressIndicatorApp.directive('ngArc', function(){
 				.text(function(d){
 					return attrs.actual*100 + "\%";
 				})
-				.attr("font-size","30px")
+				.attr("font-size","20px")
 				.attr("text-anchor", "middle")
-				.attr("y",+20)
-			svg_actual.append("text").text("actual")
+				.attr("y",0)
+			svg_actual.append("text").text("progress")
+				.attr("font-size","15px")
 				.attr("text-anchor", "middle")
-				.attr("y",-10);				
+				.attr("y",+10);				
 
 
-			
-			foreground_actual.transition().duration(1000).call(arcTween, attrs.actual*τ);
+			foreground_expected.transition().duration(1000).call(arcTween_expected, attrs.expected*τ);
+			foreground_actual.transition().duration(1000).call(arcTween_actual, attrs.actual*τ);
 
 			// arctween: http://bl.ocks.org/mbostock/5100636
-			function arcTween(transition, newAngle) {
+			function arcTween_expected(transition, newAngle) {
 			  transition.attrTween("d", function(d) {
 			    var interpolate = d3.interpolate(d.endAngle, newAngle);
 			    return function(t) {
 			      d.endAngle = interpolate(t);
-			      return arc(d);
+			      return arc_expected(d);
+			    };
+			  });
+			}
+
+			function arcTween_actual(transition, newAngle) {
+			  transition.attrTween("d", function(d) {
+			    var interpolate = d3.interpolate(d.endAngle, newAngle);
+			    return function(t) {
+			      d.endAngle = interpolate(t);
+			      return arc_actual(d);
 			    };
 			  });
 			}
