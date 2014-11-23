e7c6bf5ed7c646855ccefdc7a7522dc308978123
vivianliang <vliang88@gmail.com>
2014-11-23 16:22:18 -0600

    directive unit test structure. fails

--- app/app_test.js
+++ app/app_test.js
@@ -17,6 +17,34 @@ describe('progressIndicatorApp module', function() {
     	}));
 
   	});
+
+
+	describe('directive: ngArc', function(){
+		var element, scope;
+
+		beforeEach(module('progressIndicatorApp'));
+
+		beforeEach(inject(function($rootScope, $compile) {
+	    	scope = $rootScope.$new();
+	    	element=
+	    		'<ng-arc actual="{{actual}}" expected="1"></ng-arc>';
+	    	scope.actual=.75;
+	    	element = $compile(element)(scope);
+    		scope.$digest();
+		}));
+
+		describe('with the first given value', function() {
+		  it("should compute the size to create other values", function() {
+		    //var isolated = element.isolateScope();
+		    //expect(isolated.values.ele_width).toBe(200);
+		    //expect(isolated.values.ele_height).toBe(200);
+		    expect(element.attr('actual')).toBe('.75');
+		    expect(element.attr('expected')).toBe('1');
+		  });
+		});
+
+	});
+
 });
 
 // TODO: tests for green, orange, red based on expected and actual
